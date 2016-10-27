47aa1ea322527757bf71c5447a2d15717303b3ca
arthu <arthur.oganezov@gmail.com>
2016-10-27 00:32:50 -0500

    drop down options donegit add index.html indexJS.jsgit add index.html indexJS.js!

--- indexJS.js
+++ indexJS.js
@@ -23,6 +23,7 @@ $(document).ready(function() {
             var items = [];
             var selectedId = $('.makes').find(':selected').attr('id');
             var makes = data["makes"];
+            console.log(makes);
             for (var i = 0; i < makes.length; ++i) {
 
                if (makes[i]['id'] == selectedId) {
@@ -37,24 +38,64 @@ $(document).ready(function() {
             $(".models").append(items.join("" ));
         });
     });
-    $( ".makes" ).change(function() {
+
+    $( ".models" ).change(function() {
 
         $.getJSON( "makes.json", function( data ) {
             var items = [];
-            //var selectedId = $('.makes').find(':selected').attr('id');
-            var selectedModel = $('makes').find(':selected').attr('years');
+            var selectedId = $('.makes').find(':selected').attr('id');
             var makes = data["makes"];
             for (var i = 0; i < makes.length; ++i) {
-                if (makes[i]['models'] == selectedModel) {
+                if (makes[i]['id'] == selectedId) {
+                    var models = makes[i]['models'];
+                    var selectedModel = $('.models').find(':selected').attr('id');
+                    console.log(models);
+                    for(var x = 0; x < models.length; ++x) {
+                        console.log("x: "+ x);
+                        if (models[x]['id'] == selectedModel) {
+                            $.each( models[x]['years'], function( key, val ) {
+                                items.push( "<option id='" + val["id"] + "'>" + val["year"] + "</option>" );
+                            });
+                            break;
+                        }
+                    }
 
-                    $.each( makes[i]['models']['years'], function( key, val ) {
-                        items.push( "<option id='" + val["id"] + "'>" + val["name"] + "</option>" );
-                    });
-                    break;
                 }
             }
             $(".years").empty();
             $(".years").append(items.join("" ));
         });
     });
+
+    /*
+    $( ".models" ).change(function() {
+
+        $.getJSON( "makes.json", function( data ) {
+            var items = [];
+            console.log(1);
+            var selectedId = $('.makes').find(':selected').attr('id');
+            console.log(2);
+            var selectedModel = $('.years').find(':selected').attr('id');
+            console.log(3);
+            console.log(data["makes"]);
+            console.log(data["makes"]["models"]);
+
+            var models = data["makes"]["models"];
+            console.log(4);
+            console.log(models);
+            console.log(5);
+            for (var i = 0; i < models.length; ++i) {
+                if (models[i]['models'] == selectedModel) {
+                    console.log(6);
+                    $.each(models[i]['models']['years'], function( key, val ) {
+                        items.push( "<option id='" + val["id"] + "'>" + val["name"] + "</option>" );
+                    });
+                    console.log(7);
+                    break;
+                }
+            }
+            $(".years").empty();
+            $(".years").append(items.join("" ));
+        });
+    });*/
 });
