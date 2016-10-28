9c92d2288653ba70b9c3a0da300f8c9fe03a4b1f
arthu <arthur.oganezov@gmail.com>
2016-10-27 21:36:27 -0500

    It sort of looks good now.

--- indexJS.js
+++ indexJS.js
@@ -49,9 +49,7 @@ $(document).ready(function() {
                 if (makes[i]['id'] == selectedId) {
                     var models = makes[i]['models'];
                     var selectedModel = $('.models').find(':selected').attr('id');
-                    console.log(models);
                     for(var x = 0; x < models.length; ++x) {
-                        console.log("x: "+ x);
                         if (models[x]['id'] == selectedModel) {
                             $.each( models[x]['years'], function( key, val ) {
                                 items.push( "<option id='" + val["id"] + "'>" + val["year"] + "</option>" );
@@ -66,36 +64,4 @@ $(document).ready(function() {
             $(".years").append(items.join("" ));
         });
     });
-
-    /*
-    $( ".models" ).change(function() {
-
-        $.getJSON( "makes.json", function( data ) {
-            var items = [];
-            console.log(1);
-            var selectedId = $('.makes').find(':selected').attr('id');
-            console.log(2);
-            var selectedModel = $('.years').find(':selected').attr('id');
-            console.log(3);
-            console.log(data["makes"]);
-            console.log(data["makes"]["models"]);
-
-            var models = data["makes"]["models"];
-            console.log(4);
-            console.log(models);
-            console.log(5);
-            for (var i = 0; i < models.length; ++i) {
-                if (models[i]['models'] == selectedModel) {
-                    console.log(6);
-                    $.each(models[i]['models']['years'], function( key, val ) {
-                        items.push( "<option id='" + val["id"] + "'>" + val["name"] + "</option>" );
-                    });
-                    console.log(7);
-                    break;
-                }
-            }
-            $(".years").empty();
-            $(".years").append(items.join("" ));
-        });
-    });*/
 });
